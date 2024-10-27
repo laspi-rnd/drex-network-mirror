2a9d8c67e7e2413b3429d3eeb2d60378477834b0
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2024-10-27 16:06:04 -0300

    fixing network startup

--- mainnet/scripts/cleanup.sh
+++ mainnet/scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 docker compose -f network/docker-compose-mainnet.yaml down 
 
-echo "Cleaning up data"
-for i in {1..8}; do
-  rm -rf network/nodes/node$i/data/*
-done
+#echo "Cleaning up data"
+#for i in {1..8}; do
+#  rm -rf network/nodes/node$i/data/*
+#done
